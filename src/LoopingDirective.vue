56259237cb253f041d5babc1c3ea92ae1c0ca4a4
MianwaliCodeHouse <mianwalicodehouse@gmail.com>
2024-12-01 13:13:20 +0500

    save 5th

---
+++ src/LoopingDirective.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app-body">
+    <h1 v-for="number in numbers">{{ number }}</h1>
+  </div>
+</template>
+<script>
+  export default{
+    data(){
+      return {
+        numbers : [11,22,33,44]
+      }
+    }
+  }
+</script>
+<style>
+.app-body{
+  text-align: center;
+  padding: 30px;
+}
+</style>
+
+
